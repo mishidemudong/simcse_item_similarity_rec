f6347d7777d453f9a8002bb135b6353687c2d6fe
mishidemudong <liang.dongkai@163.com>
2021-05-24 18:24:42 +0800

    add first code

---
+++ train_similarity_simcse.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 24 15:35:34 2021
+
+@author: liang
+"""
+
+from utils import *
+import sys
+import os
+import tensorflow as tf
+from bert4keras.optimizers import Adam
+from bert4keras.snippets import DataGenerator, sequence_padding
+import jieba
+jieba.initialize()
+
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+###set gpu memory
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.compat.v1.Session(config=config)
+
+# 基本参数
+model_type=['SimBERT-tiny', 'SimBERT-small'][0]
+pooling = ['first-last-avg', 'last-avg', 'cls', 'pooler'][0]
+task_name = ['ATEC', 'BQ', 'LCQMC'][1]
+dropout_rate = 0.1
+
+maxlen = 64
+
+# 加载数据集
+data_path = '/media/liang/Nas/corpus/文本相似度/chn/senteval_cn/'
+
+datasets = {
+    '%s-%s' % (task_name, f):
+    load_data('%s%s/%s.%s.data' % (data_path, task_name, task_name, f))
+    for f in ['train', 'valid', 'test']
+}
+
+# bert配置
+model_name = {
+    'SimBERT-tiny': 'chinese_simbert_L-4_H-312_A-12',
+    'SimBERT-small': 'chinese_simbert_L-6_H-384_A-12'
+}[model_type]
+
+config_path = '/media/liang/Nas/PreTrainModel/simbert/chinese_simbert_L-4_H-312_A-12/bert_config.json' 
+checkpoint_path = '/media/liang/Nas/PreTrainModel/simbert/chinese_simbert_L-4_H-312_A-12/bert_model.ckpt' 
+dict_path = '/media/liang/Nas/PreTrainModel/simbert/chinese_simbert_L-4_H-312_A-12/vocab.txt' 
+
+# 建立分词器
+tokenizer = get_tokenizer(dict_path)
+
+# 建立模型
+if model_type == 'RoFormer':
+    encoder = get_encoder(
+        config_path,
+        checkpoint_path,
+        model='roformer',
+        pooling=pooling,
+        dropout_rate=dropout_rate
+    )
+elif 'NEZHA' in model_type:
+    encoder = get_encoder(
+        config_path,
+        checkpoint_path,
+        model='nezha',
+        pooling=pooling,
+        dropout_rate=dropout_rate
+    )
+else:
+    encoder = get_encoder(
+        config_path,
+        checkpoint_path,
+        pooling=pooling,
+        dropout_rate=dropout_rate
+    )
+
+# 语料id化
+all_names, all_weights, all_token_ids, all_labels = [], [], [], []
+train_token_ids = []
+for name, data in datasets.items():
+    a_token_ids, b_token_ids, labels = convert_to_ids(data, tokenizer, maxlen)
+    all_names.append(name)
+    all_weights.append(len(data))
+    all_token_ids.append((a_token_ids, b_token_ids))
+    all_labels.append(labels)
+    train_token_ids.extend(a_token_ids)
+    train_token_ids.extend(b_token_ids)
+
+
+
+class data_generator(DataGenerator):
+    """训练语料生成器
+    """
+    def __iter__(self, random=False):
+        batch_token_ids = []
+        for is_end, token_ids in self.sample(random):
+            batch_token_ids.append(token_ids)
+            batch_token_ids.append(token_ids)
+            if len(batch_token_ids) == self.batch_size * 2 or is_end:
+                batch_token_ids = sequence_padding(batch_token_ids)
+                batch_segment_ids = np.zeros_like(batch_token_ids)
+                batch_labels = np.zeros_like(batch_token_ids[:, :1])
+                yield [batch_token_ids, batch_segment_ids], batch_labels
+                batch_token_ids = []
+
+
+def simcse_loss(y_true, y_pred):
+    """用于SimCSE训练的loss
+    """
+    # 构造标签
+    idxs = K.arange(0, K.shape(y_pred)[0])
+    idxs_1 = idxs[None, :]
+    idxs_2 = (idxs + 1 - idxs % 2 * 2)[:, None]
+    y_true = K.equal(idxs_1, idxs_2)
+    y_true = K.cast(y_true, K.floatx())
+    # 计算相似度
+    y_pred = K.l2_normalize(y_pred, axis=1)
+    similarities = K.dot(y_pred, K.transpose(y_pred))
+    similarities = similarities - tf.eye(K.shape(y_pred)[0]) * 1e12
+    similarities = similarities * 20
+    loss = K.categorical_crossentropy(y_true, similarities, from_logits=True)
+    return K.mean(loss)
+
+
+# SimCSE训练
+encoder.summary()
+encoder.compile(loss=simcse_loss, optimizer=Adam(1e-5))
+train_generator = data_generator(train_token_ids, 64)
+encoder.fit(
+    train_generator.forfit(), steps_per_epoch=len(train_generator), epochs=1
+)
+
+# 语料向量化
+all_vecs = []
+for a_token_ids, b_token_ids in all_token_ids:
+    a_vecs = encoder.predict([a_token_ids,
+                              np.zeros_like(a_token_ids)],
+                             verbose=True)
+    b_vecs = encoder.predict([b_token_ids,
+                              np.zeros_like(b_token_ids)],
+                             verbose=True)
+    all_vecs.append((a_vecs, b_vecs))
+
+# 标准化，相似度，相关系数
+all_corrcoefs = []
+for (a_vecs, b_vecs), labels in zip(all_vecs, all_labels):
+    a_vecs = l2_normalize(a_vecs)
+    b_vecs = l2_normalize(b_vecs)
+    sims = (a_vecs * b_vecs).sum(axis=1)
+    corrcoef = compute_corrcoef(labels, sims)
+    all_corrcoefs.append(corrcoef)
+
+all_corrcoefs.extend([
+    np.average(all_corrcoefs),
+    np.average(all_corrcoefs, weights=all_weights)
+])
+
+for name, corrcoef in zip(all_names + ['avg', 'w-avg'], all_corrcoefs):
+    print('%s: %s' % (name, corrcoef))

be09138c033af2c5fb8f9657560eb7cc67d296ed
mishidemudong <liang.dongkai@163.com>
2021-05-24 19:36:07 +0800

    add predict code

--- train_similarity_simcse.py
+++ train_similarity_simcse.py
@@ -13,10 +13,10 @@ import tensorflow as tf
 from bert4keras.optimizers import Adam
 from bert4keras.snippets import DataGenerator, sequence_padding
 import jieba
+
 jieba.initialize()
 
-
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -26,11 +26,11 @@ config.gpu_options.allow_growth = True
 sess = tf.compat.v1.Session(config=config)
 
 # 基本参数
-model_type=['SimBERT-tiny', 'SimBERT-small'][0]
+model_type = ['SimBERT-tiny', 'SimBERT-small'][0]
 pooling = ['first-last-avg', 'last-avg', 'cls', 'pooler'][0]
 task_name = ['ATEC', 'BQ', 'LCQMC'][1]
 dropout_rate = 0.1
-
+epoch = 10
 maxlen = 64
 
 # 加载数据集
@@ -38,7 +38,7 @@ data_path = '/media/liang/Nas/corpus/文本相似度/chn/senteval_cn/'
 
 datasets = {
     '%s-%s' % (task_name, f):
-    load_data('%s%s/%s.%s.data' % (data_path, task_name, task_name, f))
+        load_data('%s%s/%s.%s.data' % (data_path, task_name, task_name, f))
     for f in ['train', 'valid', 'test']
 }
 
@@ -48,9 +48,9 @@ model_name = {
     'SimBERT-small': 'chinese_simbert_L-6_H-384_A-12'
 }[model_type]
 
-config_path = '/media/liang/Nas/PreTrainModel/simbert/chinese_simbert_L-4_H-312_A-12/bert_config.json' 
-checkpoint_path = '/media/liang/Nas/PreTrainModel/simbert/chinese_simbert_L-4_H-312_A-12/bert_model.ckpt' 
-dict_path = '/media/liang/Nas/PreTrainModel/simbert/chinese_simbert_L-4_H-312_A-12/vocab.txt' 
+config_path = '/media/liang/Nas/PreTrainModel/simbert/chinese_simbert_L-4_H-312_A-12/bert_config.json'
+checkpoint_path = '/media/liang/Nas/PreTrainModel/simbert/chinese_simbert_L-4_H-312_A-12/bert_model.ckpt'
+dict_path = '/media/liang/Nas/PreTrainModel/simbert/chinese_simbert_L-4_H-312_A-12/vocab.txt'
 
 # 建立分词器
 tokenizer = get_tokenizer(dict_path)
@@ -93,10 +93,10 @@ for name, data in datasets.items():
     train_token_ids.extend(b_token_ids)
 
 
-
 class data_generator(DataGenerator):
     """训练语料生成器
     """
+
     def __iter__(self, random=False):
         batch_token_ids = []
         for is_end, token_ids in self.sample(random):
@@ -133,7 +133,7 @@ encoder.summary()
 encoder.compile(loss=simcse_loss, optimizer=Adam(1e-5))
 train_generator = data_generator(train_token_ids, 64)
 encoder.fit(
-    train_generator.forfit(), steps_per_epoch=len(train_generator), epochs=1
+    train_generator.forfit(), steps_per_epoch=len(train_generator), epochs=epoch
 )
 
 # 语料向量化
@@ -163,3 +163,34 @@ all_corrcoefs.extend([
 
 for name, corrcoef in zip(all_names + ['avg', 'w-avg'], all_corrcoefs):
     print('%s: %s' % (name, corrcoef))
+
+
+def std_simi_cal_func(a_vecs, b_vecs):
+    a_vecs = l2_normalize(a_vecs)
+    b_vecs = l2_normalize(b_vecs)
+    sims = (a_vecs * b_vecs).sum(axis=1)
+    corrcoef = compute_corrcoef(labels, sims)
+
+    return corrcoef
+
+
+def cal_predict(fea_data, encoder, item_id):
+    result = {}
+    result['item_id'] = item_id
+    item_other = fea_data.keys()
+    vecs_list = []
+    a_token_ids = tokenizer.encode(fea_data[item_id], maxlen=maxlen)[0]
+    a_vecs = encoder.predict([a_token_ids,
+                              np.zeros_like(a_token_ids)],
+                             verbose=True)
+    for item_o in fea_data.keys():
+        b_token_ids = tokenizer.encode(dst_b, maxlen=maxlen)[0]
+        b_vecs = encoder.predict([b_token_ids,
+                                  np.zeros_like(b_token_ids)],
+                                 verbose=True)
+
+        vecs_list.append()
+
+
+
+

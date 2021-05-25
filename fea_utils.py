7262d55582eea4a92c5e24c9121392934ae4438b
mishidemudong <liang.dongkai@163.com>
2021-05-25 14:12:46 +0800

    add predict 3333

---
+++ fea_utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 25 14:01:24 2021
+
+@author: liang
+"""
+
+fea_name = {
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+fea_numeric = []
+
+fea_text = []
